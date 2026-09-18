12edf68746af5987d27355c9755a3b5d029b186b
MKoblents <koblentsmaria@gmail.com>
2026-09-18 16:48:44 +0300

    redraw canvas before adding new point

--- script.js
+++ script.js
@@ -17,36 +17,36 @@ function numberRelativeToR(n, r){
 function toCanvasY(y){
     return centerY - y*scale;
 }
-function drawCanvas(R){
-    ctx.fillStyle='rgba(52,152,219,0.7)';
+function drawCanvas(R) {
+    ctx.fillStyle = 'rgba(52,152,219,0.7)';
     ctx.fillRect(
         toCanvasX(-R),
         toCanvasY(R),
-        R*scale,
-        R*scale
+        R * scale,
+        R * scale
     );
 
     ctx.beginPath();
     ctx.moveTo(toCanvasX(0), toCanvasY(0));
-    ctx.lineTo(toCanvasX(R/2), toCanvasY(0));
-    ctx.lineTo(toCanvasX(0), toCanvasY(R/2));
+    ctx.lineTo(toCanvasX(R / 2), toCanvasY(0));
+    ctx.lineTo(toCanvasX(0), toCanvasY(R / 2));
     ctx.closePath();
-    ctx.fillStyle='rgba(52,152,219,0.7)';
+    ctx.fillStyle = 'rgba(52,152,219,0.7)';
     ctx.fill();
 
     ctx.beginPath();
     ctx.moveTo(toCanvasX(0), toCanvasY(0));
-    ctx.lineTo(toCanvasX(R/2), toCanvasY(0));
+    ctx.lineTo(toCanvasX(R / 2), toCanvasY(0));
     ctx.arc(
         toCanvasX(0),
         toCanvasY(0),
-        (R/2)*scale,
+        (R / 2) * scale,
         0,
-        Math.PI/2,
+        Math.PI / 2,
         false
     )
     ctx.closePath();
-    ctx.fillStyle='rgba(52,152,219,0.7)';
+    ctx.fillStyle = 'rgba(52,152,219,0.7)';
     ctx.fill();
 
     ctx.beginPath();
@@ -74,51 +74,52 @@ function drawCanvas(R){
     ctx.lineTo(centerX, 0);
     ctx.lineTo(centerX + 5, 10);
     ctx.stroke();
+
+
+    ctx.font = '20px Arial';
+    ctx.fillStyle = 'black';
+    ctx.fillText('X', width - 20, centerY - 10);
+    ctx.fillText('Y', centerX - 20, 20);
+    const xMarks = [-R, -R / 2, R / 2, R];
+    ctx.font = '20px Arial';
+    ctx.textAlign = 'center';
+
+    xMarks.forEach(function (x) {
+        const canvasX = toCanvasX(x);
+        ctx.beginPath();
+        ctx.moveTo(canvasX, centerY - 5);
+        ctx.lineTo(canvasX, centerY + 5);
+        ctx.stroke();
+
+        let label = x;
+        if (x === -R) label = '-R';
+        else if (x === -R / 2) label = '-R/2';
+        else if (x === R) label = 'R';
+        else label = 'R/2';
+
+        ctx.fillText(label, canvasX, centerY + 20);
+    });
+    const yMarks = [-R, -R / 2, R / 2, R];
+    ctx.textAlign = 'right';
+
+    yMarks.forEach(function (y) {
+        const canvasY = toCanvasY(y);
+
+        ctx.beginPath();
+        ctx.moveTo(centerX - 5, canvasY);
+        ctx.lineTo(centerX + 5, canvasY);
+        ctx.stroke();
+
+        let label = y;
+        if (y === -R) label = '-R';
+        else if (y === -R / 2) label = '-R/2';
+        else if (y === R / 2) label = 'R/2';
+        else if (y === R) label = 'R';
+
+        ctx.fillText(label, centerX - 10, canvasY + 5);
+    });
 }
 drawCanvas(R);
-
-ctx.font = '20px Arial';
-ctx.fillStyle = 'black';
-ctx.fillText('X', width - 20, centerY - 10);
-ctx.fillText('Y', centerX -20, 20);
-const xMarks = [-R, -R/2, R/2, R];
-ctx.font = '20px Arial';
-ctx.textAlign = 'center';
-
-xMarks.forEach(function(x) {
-    const canvasX = toCanvasX(x);
-    ctx.beginPath();
-    ctx.moveTo(canvasX, centerY - 5);
-    ctx.lineTo(canvasX, centerY + 5);
-    ctx.stroke();
-
-    let label = x;
-    if (x === -R) label = '-R';
-    else if (x === -R / 2) label = '-R/2';
-    else if (x === R) label = 'R';
-    else label = 'R/2';
-
-    ctx.fillText(label, canvasX, centerY + 20);
-});
-const yMarks = [-R, -R/2, R/2, R];
-ctx.textAlign = 'right';
-
-yMarks.forEach(function(y) {
-    const canvasY = toCanvasY(y);
-
-    ctx.beginPath();
-    ctx.moveTo(centerX - 5, canvasY);
-    ctx.lineTo(centerX + 5, canvasY);
-    ctx.stroke();
-
-    let label = y;
-    if (y === -R) label = '-R';
-    else if (y === -R/2) label = '-R/2';
-    else if (y === R/2) label = 'R/2';
-    else if (y === R) label = 'R';
-
-    ctx.fillText(label, centerX - 10, canvasY + 5);
-});
 
 const y = document.getElementById('y-input').value;
 const selectedX = document.querySelector('input[name="x"]:checked');
@@ -167,6 +168,8 @@ form.addEventListener('submit',async  function (event){
         const rNum = parseFloat(r);
 
         const isHit = checkHit(xNum, yNum, rNum);
+        ctx.clearRect(0, 0, width, height);
+        drawCanvas(R);
         drawPoint(xNum, yNum, rNum, isHit);
         const result = {
             x: xNum,
@@ -183,6 +186,7 @@ form.addEventListener('submit',async  function (event){
 });
 document.addEventListener('DOMContentLoaded', async () => {
     await loadFromLocalStorage();
+    ctx.clearRect(0, 0, width, height);
     drawCanvas(R);
 });
 function checkHit(x, y, R) {
@@ -227,7 +231,7 @@ function drawPoint(x, y, R, isHit) {
     ctx.stroke();
 }
 function redrawCanvas(R) {
-    ctx.clearRect(0, 0, width, height);
+    ctx.clearRect(0, 0, width, height0, 0, width, height);
 
     ctx.beginPath();
     ctx.moveTo(0, centerY);
